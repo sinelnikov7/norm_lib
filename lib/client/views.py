393d2a9bed9c7ebcf15ccf3345dd18fb21b47246
yauhen mikhailik <yauhenmikhailik@icloud.com>
2022-06-09 22:49:52 +0300

    не проходит проверка на повтор пароля

--- lib/client/views.py
+++ lib/client/views.py
@@ -10,13 +10,20 @@ def authorization(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            # form.save()
-            user = User.objects.create_user(username=request.POST.get('username'),first_name=request.POST.get('first_name'),last_name=request.POST.get('last_name'),email=request.POST.get('email'),password=request.POST.get('password'))
-            user.save()
+            if request.POST.get('password') == request.POST.get('password_again'):
+                user = User.objects.create_user(username=request.POST.get('username'),first_name=request.POST.get('first_name'),last_name=request.POST.get('last_name'),email=request.POST.get('email'),password=request.POST.get('password'))
+                user.save()
+            else:
+                error = 'Пароль не совпадает'
+                context = {
+                    'form': form,
+                    'error':error
+
+                }
+                return render(request, 'authorization.html', context)
         else:
             context ={
                 'form':form
-
             }
             return render(request, 'authorization.html', context)
 
