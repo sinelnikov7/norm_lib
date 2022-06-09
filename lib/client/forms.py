0ea6b17b8c5126d56e3e2edc10c692db79ce2271
yauhen mikhailik <yauhenmikhailik@icloud.com>
2022-06-09 22:25:48 +0300

    регистрация нового пользователя

--- lib/client/forms.py
+++ lib/client/forms.py
@@ -1,7 +1,37 @@
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 
 class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password']
+
+        help_texts = {
+            'username': None,
+        }
+
+        widgets = {
+            'username': TextInput(attrs={
+                'class':'label_style',
+                'placeholder':'Введите логин'
+            }),
+            'first_name': TextInput(attrs={
+                'class': 'label_style',
+                'placeholder': 'Имя'
+            }),
+            'last_name': TextInput(attrs={
+                'class': 'label_style',
+                'placeholder': 'Фамилия'
+            }),
+            'email': TextInput(attrs={
+                'class': 'label_style',
+                'placeholder': 'Email',
+                'type':'email'
+            }),
+            'password': TextInput(attrs={
+                'class': 'label_style',
+                'placeholder': 'Пароль',
+                'type': 'password'
+            })
+
+        }
--- lib/client/views.py
+++ lib/client/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from .forms import UserForm
 
 def authorization(request):
@@ -9,7 +10,9 @@ def authorization(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            form.save()
+            # form.save()
+            user = User.objects.create_user(username=request.POST.get('username'),first_name=request.POST.get('first_name'),last_name=request.POST.get('last_name'),email=request.POST.get('email'),password=request.POST.get('password'))
+            user.save()
         else:
             context ={
                 'form':form
